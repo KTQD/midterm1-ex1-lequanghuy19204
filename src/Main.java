89d839ad4a271ab0c7d11d4909f41725dcfe7e26
lequanghuy19204 <152720236+lequanghuy19204@users.noreply.github.com>
2024-03-14 07:37:23 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
-        MP3 mp3;
+        MP3 mp3 = new MP3("Chung ta khhong thuc ve nhau", "bai nay hay", "12345", 23.5f, 10);
+        Book book = new Book("Dune 1", "Khoa hoc vien tuong", "23456", 50.5f, "huy", 400, "Khoa hoc vien tuong");
         mp3.showInfo();
-
-        Book book;
+        System.out.println();
         book.showInfo();
-
     }
 }
