de250c340d4e01cc2f5962a9ea3f970306425229
lequanghuy19204 <152720236+lequanghuy19204@users.noreply.github.com>
2024-03-14 07:35:18 +0700

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,24 @@
-public class Book {
+public class Book extends Item {
+    private String author;
+    private int numberOfPages;
+    private String genre;
+
+    public Book(String name, String description, String ID, float price, String author, int numberOfPages, String genre) {
+        super(name, description, ID, price);
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+        this.genre = genre;
+    }
+
+    @Override
+    void showInfo() {
+        System.out.println("Thong tin sach:");
+        System.out.println("Ten: " + name);
+        System.out.println("Mieu ta sach: " + getDescription());
+        System.out.println("ID: " + ID);
+        System.out.println("Gia: " + price);
+        System.out.println("Tac gia: " + author);
+        System.out.println("So trang: " + numberOfPages);
+        System.out.println("The loai: " + genre);
+    }
 }
